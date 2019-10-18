f26e4e4a7a6099dc7d1dc4c762c92de6e4154f52
zhangsan <884707993@qq.com>
2019-10-18 10:20:25 +0000

    BUG

--- app/Http/Controllers/Index/IndexController.php
+++ app/Http/Controllers/Index/IndexController.php
@@ -28,7 +28,7 @@ class IndexController extends Controller
 
             $week_newshop = DB::table('mt_shop')->orderBy('shop_add_time')->limit(3)->get(['shop_id','shop_name','shop_Ename','shop_desc'])->toArray();    //本周新店
             //var_dump($week_newshop);exit;
-            $recommend = DB::table('mt_goods')->where(['is_recommend'=>1])->get(['goods_id','price','picture']);
+            $recommend = DB::table('mt_goods')->where(['is_recommend'=>1])->get(['goods_id','price','picture']);       //推荐
             //var_dump($recommend);exit;
 
 //            $data = [
