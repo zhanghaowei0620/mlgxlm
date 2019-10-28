25a9b0113f535f719502930e20aa45dcd6ad638c
zhangsan <884707993@qq.com>
2019-10-28 09:51:43 +0000

    a

--- app/Http/Controllers/Index/IndexController.php
+++ app/Http/Controllers/Index/IndexController.php
@@ -11,7 +11,8 @@ use yii\console\widgets\Table;
 class IndexController extends Controller
 {
     //获取首页数据
-    public function index(){
+    public function index(Request $request){
+        $promotion_type = $request->input('promotion_type');
         $type = DB::table('mt_type')->where(['p_id'=>0])->get()->toArray();  //父级分类
         $s_type1 = DB::table('mt_type')->where(['p_id'=>1])->get()->toArray();          //子集分类 第一行
         $s_type2 = DB::table('mt_type')->where(['p_id'=>2])->get()->toArray();         //子集分类 第二行
@@ -19,35 +20,68 @@ class IndexController extends Controller
         $s_type4 = DB::table('mt_type')->where(['p_id'=>4])->get()->toArray();          //子集分类 第二行
         //var_dump($s_type4);
 
-        $goodsInfo = DB::table('mt_goods')
-            ->join('mt_shop','mt_goods.shop_id','=','mt_shop.shop_id')
-            ->where(['promotion_type'=>1])
-            ->get(['shop_name','shop_address_provice','shop_address_city','shop_address_area','shop_score','goods_id','goods_name','price','market_price','introduction','picture'])->toArray();   //店铺精选   默认为1
-        //var_dump($goodsInfo);exit;
+        if($promotion_type){
+            $goodsInfo = DB::table('mt_goods')
+                ->join('mt_shop','mt_goods.shop_id','=','mt_shop.shop_id')
+                ->where(['promotion_type'=>$promotion_type])
+                ->get(['shop_name','shop_address_provice','shop_address_city','shop_address_area','shop_score','goods_id','goods_name','price','market_price','introduction','picture'])->toArray();   //店铺精选   默认为1
+            //var_dump($goodsInfo);exit;
 
 
-        $week_newshop = DB::table('mt_shop')->orderBy('shop_add_time')->limit(3)->get(['shop_id','shop_name','shop_Ename','shop_desc'])->toArray();    //本周新店
-        //var_dump($week_newshop);exit;
-        $recommend = DB::table('mt_goods')->where(['is_recommend'=>1])->get(['goods_id','price','picture']);       //推荐
-        //var_dump($recommend);exit;
+            $week_newshop = DB::table('mt_shop')->orderBy('shop_add_time')->limit(3)->get(['shop_id','shop_name','shop_Ename','shop_desc'])->toArray();    //本周新店
+            //var_dump($week_newshop);exit;
+            $recommend = DB::table('mt_goods')->where(['is_recommend'=>1])->get(['goods_id','price','picture']);       //推荐
+            //var_dump($recommend);exit;
 
-        $data = [
-            'type'          =>  $type,
-            's_type1'      =>  $s_type1,
-            's_type2'      =>  $s_type2,
-            's_type3'      =>  $s_type3,
-            's_type4'      =>  $s_type4,
-            'goodsInfo'     =>  $goodsInfo,
-            'week_newshop'  =>  $week_newshop,
-            'recommend'     =>  $recommend,
-            'code'         =>  0
-        ];
+            $data = [
+                'type'          =>  $type,
+                's_type1'      =>  $s_type1,
+                's_type2'      =>  $s_type2,
+                's_type3'      =>  $s_type3,
+                's_type4'      =>  $s_type4,
+                'goodsInfo'     =>  $goodsInfo,
+                'week_newshop'  =>  $week_newshop,
+                'recommend'     =>  $recommend,
+                'code'         =>  0
+            ];
 
-        $response = [
-            'data'=>$data
-        ];
-        //var_dump($response);exit;
-        return json_encode($response,JSON_UNESCAPED_UNICODE);
+            $response = [
+                'data'=>$data
+            ];
+            //var_dump($response);exit;
+            return json_encode($response,JSON_UNESCAPED_UNICODE);
+        }else{
+            $goodsInfo = DB::table('mt_goods')
+                ->join('mt_shop','mt_goods.shop_id','=','mt_shop.shop_id')
+                ->where(['promotion_type'=>1])
+                ->get(['shop_name','shop_address_provice','shop_address_city','shop_address_area','shop_score','goods_id','goods_name','price','market_price','introduction','picture'])->toArray();   //店铺精选   默认为1
+            //var_dump($goodsInfo);exit;
+
+
+            $week_newshop = DB::table('mt_shop')->orderBy('shop_add_time')->limit(3)->get(['shop_id','shop_name','shop_Ename','shop_desc'])->toArray();    //本周新店
+            //var_dump($week_newshop);exit;
+            $recommend = DB::table('mt_goods')->where(['is_recommend'=>1])->get(['goods_id','price','picture']);       //推荐
+            //var_dump($recommend);exit;
+
+            $data = [
+                'type'          =>  $type,
+                's_type1'      =>  $s_type1,
+                's_type2'      =>  $s_type2,
+                's_type3'      =>  $s_type3,
+                's_type4'      =>  $s_type4,
+                'goodsInfo'     =>  $goodsInfo,
+                'week_newshop'  =>  $week_newshop,
+                'recommend'     =>  $recommend,
+                'code'         =>  0
+            ];
+
+            $response = [
+                'data'=>$data
+            ];
+            //var_dump($response);exit;
+            return json_encode($response,JSON_UNESCAPED_UNICODE);
+        }
+
     }
 
 
