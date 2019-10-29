702a892d1e8508795dd835831c199b0c484df27f
zhangsan <884707993@qq.com>
2019-10-29 10:01:22 +0000

    a

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -323,7 +323,7 @@ class UserController extends Controller
             $coupon_num = DB::table('mt_user')
                 ->join('mt_coupon','mt_user.uid','=','mt_coupon.uid')
                 ->where('mt_user.openid',$openid)->get()->count();
-            var_dump($coupon_num);exit;
+            //var_dump($coupon_num);exit;
             if($userInfo){
                 if($coupon_num){
                     $data = [
