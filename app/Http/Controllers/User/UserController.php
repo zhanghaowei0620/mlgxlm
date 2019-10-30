ed042945aab47dca230d2e8d3ee74dd95f12d35a
zhangsan <884707993@qq.com>
2019-10-30 07:24:34 +0000

    a

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -500,7 +500,7 @@ class UserController extends Controller
                 'shop_address_detail'=>$shop_address_detail,
                 'shop_add_time'=>$shop_add_time,
                 'shop_contacts'=>$contacts,
-                'goods_status'=>2,
+                'shop_status'=>2,
                 'uid'=>$uid
             ];
             $where = [
