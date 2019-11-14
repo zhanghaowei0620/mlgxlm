8cbcdfa3858a08f765125af1ea4ae7839efc239a
root <root@localhost.localdomain>
2019-11-14 14:23:44 +0800

    aaa

--- app/Http/Controllers/Index/IndexController.php
+++ app/Http/Controllers/Index/IndexController.php
@@ -77,7 +77,7 @@ class IndexController extends Controller
                 ->join('mt_type','mt_goods.t_id','=','mt_type.t_id')
                 ->where(['promotion_type'=>1],['mt_shop.shop_address_city'=>$shop_address_city])
                 ->get(['mt_goods.goods_id','goods_name','goods_type','market_price','mt_goods.price','picture','description','mt_shop.shop_name','mt_goods.prople','promotion_price','mt_type.t_name'])->toArray();
-            var_dump($goodsInfo);exit;
+//            var_dump($goodsInfo);exit;
             $shop_id = DB::table('mt_shop')
                 ->join('mt_goods','mt_goods.shop_id','=','mt_shop.shop_id')
 //                ->where(['mt_shop.shop_address_city'=>$shop_address_city])
