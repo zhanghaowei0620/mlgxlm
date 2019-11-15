42c01b94ed756d3c433ed3f4c21d01c6c882e8ae
zhangsan <884707993@qq.com>
2019-11-15 01:57:52 +0000

    d

--- app/Http/Controllers/Admin/Admin_loginController.php
+++ app/Http/Controllers/Admin/Admin_loginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Redis;
 use Storage;
 
 class Admin_loginController extends Controller
