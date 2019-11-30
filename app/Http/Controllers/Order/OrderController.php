0f41406a5105224692c2295efbc51cafa13a0ff0
root <root@localhost.localdomain>
2019-11-30 16:43:58 +0800

    aa

--- app/Http/Controllers/Order/OrderController.php
+++ app/Http/Controllers/Order/OrderController.php
@@ -146,6 +146,7 @@ class OrderController extends Controller
         $openid1 = $request->input('openid');
         $key = $openid1;
         $openid = Redis::get($key);
+        $openid='o9VUc5MWyq5GgW3kF_90NnrQkBH8';
 //        $total_price = 100;   //总价
         $order_no = date("YmdHis",time()).rand(1000,9999);   //订单号
         if($openid){
@@ -186,7 +187,8 @@ class OrderController extends Controller
                         'order_status'=>0,
                         'order_method'=>1,
                         'total_price'=>$total_price,
-                        'create_time'=>time()
+                        'create_time'=>time(),
+                        'good_cate'=>$good_cate
                     ];
                     $infodata =DB::table('mt_order')->insert($data_order);
 
@@ -239,7 +241,7 @@ class OrderController extends Controller
                             'order_status'=>0,
                             'shop_id'=>$v->shop_id,
                             'shop_name'=>$v->shop_name,
-                            'create_time'=>time()
+                            'create_time'=>time(),
                         ];
                         $datailData = DB::table('mt_order_detail')->insert($info);
                     }
@@ -263,7 +265,8 @@ class OrderController extends Controller
                     'order_status'=>0,
                     'order_method'=>1,
                     'total_price'=>$total_price,
-                    'create_time'=>time()
+                    'create_time'=>time(),
+                    'good_cate'=>$good_cate
                 ];
                 $infodata =DB::table('mt_order')->insert($data_order);
 
@@ -297,14 +300,17 @@ class OrderController extends Controller
                         'order_status'=>0,
                         'shop_id'=>$v->shop_id,
                         'shop_name'=>$v->shop_name,
-                        'create_time'=>time()
+                        'create_time'=>time(),
                     ];
                     $datailData = DB::table('mt_order_detail')->insert($info);
-                    if($datailData == true){
+                    $dainfo=DB::table('mt_order')
+                        ->where(['order_no'=>$$order_no])
+                        ->first();
+                    if($dainfo){
                         $data=[
                             'code'=>0,
                             'msg'=>'成功',
-                            'data'=>$datailData,
+                            'data'=>$datailData
                         ];
                         $response = [
                             'data'=>$data
