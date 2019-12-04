79097c08784cdc774590aeaa1f444ffb916bb2c9
zhangsan <884707993@qq.com>
2019-12-04 02:55:38 +0000

    j

--- routes/web.php
+++ routes/web.php
@@ -169,6 +169,8 @@ Route::post('index_reseller_goodsDetail', 'Reseller\ResellerController@index_res
 
 Route::post('my_team', 'Reseller\ResellerController@my_team');   //我的团队
 
+Route::get('admin_my_team', 'Amindbackstage\Headquarters@admin_my_team')->middleware('checkLogin');   //我的团队
+
 
 
 
