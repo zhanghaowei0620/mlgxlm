306d399e30aecf2b2acdd4de7f7f4b267865bb1c
zhangsan <884707993@qq.com>
2019-12-04 06:48:35 +0000

    g

--- app/Http/Controllers/Amindbackstage/Headquarters.php
+++ app/Http/Controllers/Amindbackstage/Headquarters.php
@@ -1197,7 +1197,7 @@ class Headquarters extends Controller
             $response=[
                 'code'=>0,
                 'teamInfo'=>$teamInfo,
-                'total_num'=>$teamInfo,
+                'total_num'=>$total_num,
                 'msg'=>'数据请求成功'
             ];
             return json_encode($response, JSON_UNESCAPED_UNICODE);
