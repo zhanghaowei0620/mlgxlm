d5f9f2a2b2854465bd05a12f6d2dc7a007b0896a
zhangsan <884707993@qq.com>
2019-12-10 02:40:11 +0000

    g

--- app/Http/Controllers/Invite/InviteController.php
+++ app/Http/Controllers/Invite/InviteController.php
@@ -170,7 +170,7 @@ class InviteController extends Controller
 
     }
     public function open_red_packet(Request $request){
-        $openid = $request->input('openid');
+        $openid = $request->input('openid3');
         $userInfo = DB::table('mt_user')->where('openid',$openid)->first(['uid']);
         $uid = $userInfo->uid;
         $mt_invitationInfo = DB::table('mt_invitation')->where('f_uid',$uid)->get(['uid','money','team_num'])->toArray();
