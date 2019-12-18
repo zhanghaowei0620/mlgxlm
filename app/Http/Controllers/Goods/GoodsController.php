a339308ffc07bdbebab186f3f2c0d29d3d6e01c7
root <root@localhost.localdomain>
2019-12-18 09:16:18 +0800

    aaaa

--- app/Http/Controllers/Goods/GoodsController.php
+++ app/Http/Controllers/Goods/GoodsController.php
@@ -367,7 +367,7 @@ class GoodsController extends Controller
         $openid1 = $request->input('openid');
         $key = $openid1;
         $openid = Redis::get($key);
-        $openid="o9VUc5AOsdEdOBeUAw4TdYg-F-dM";
+//        $openid="o9VUc5AOsdEdOBeUAw4TdYg-F-dM";
         if($openid){
             $buy_num = $request->input('buy_num');
             $user_info = DB::table('mt_user')->where('openid',$openid)->first();
@@ -462,7 +462,7 @@ class GoodsController extends Controller
         $openid1 = $request->input('openid');
         $key = $openid1;
         $openid = Redis::get($key);
-        $openid='o9VUc5AOsdEdOBeUAw4TdYg-F-dM';
+//        $openid='o9VUc5AOsdEdOBeUAw4TdYg-F-dM';
         $cartAdd=DB::table('mt_cart')
             ->join('mt_goods','mt_goods.goods_id','=','mt_cart.goods_id')
             ->where(['mt_cart.openid'=>$openid])
