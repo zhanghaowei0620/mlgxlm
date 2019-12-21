a4ad954a4a3add18c44ab337c74a6992df3075c1
root <root@localhost.localdomain>
2019-12-21 15:11:18 +0800

    aaa

--- app/Http/Controllers/Goods/GoodsController.php
+++ app/Http/Controllers/Goods/GoodsController.php
@@ -225,7 +225,7 @@ class GoodsController extends Controller
             ->join('mt_shop','mt_goods.shop_id','=','mt_shop.shop_id')
 //            ->join('mt_coupon','mt_coupon.goods_id','=','mt_goods.goods_id')
             ->where(['mt_shop.shop_id'=>$shop_id])
-            ->get(['mt_goods.goods_id','mt_shop.shop_id','mt_shop.shop_name','mt_goods.is_member_discount','mt_goods.coupon_redouction','mt_goods.coupon_price','mt_goods.coupon_start_time','mt_goods.picture','mt_goods.coupon_type','mt_goods.goods_name','mt_goods.coupon_names','mt_goods.picture']);
+            ->get(['mt_goods.goods_id','mt_shop.shop_id','mt_shop.shop_name','mt_goods.is_member_discount','mt_goods.coupon_redouction','mt_goods.coupon_price','mt_goods.coupon_start_time','mt_goods.picture','mt_goods.coupon_type','mt_goods.goods_name','mt_goods.coupon_names','mt_goods.picture','mt_goods.expiration']);
 //        var_dump($couponInfo);
         if($couponInfo){
             $data = [
