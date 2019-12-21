5cbec0791fa8a0cd2dca2ff6910904ed658bd45a
root <root@localhost.localdomain>
2019-12-21 16:59:11 +0800

    aa

--- app/Http/Controllers/Goods/GoodsController.php
+++ app/Http/Controllers/Goods/GoodsController.php
@@ -1114,8 +1114,7 @@ class GoodsController extends Controller
             $notify_url = 'https://mt.mlgxlm.com/test_pay';
             //dump($openid);die;
             $spbill_create_ip = $_SERVER['REMOTE_ADDR'];
-            $total_fee = $datainfo->price * 100;//因为充值金额最小是1 而且单位为分 如果是充值1元所以这里需要*100
-            var_dump($total_fee);
+            (int)$total_fee = $datainfo->price * 100;//因为充值金额最小是1 而且单位为分 如果是充值1元所以这里需要*100
             //dump($total_fee);die;
             //这里是按照顺序的 因为下面的签名是按照顺序 排序错误 肯定出错
             $post['appid'] = $appid;
