5f133402b48ca41bdbb6dff2b56444c762b32669
root <root@localhost.localdomain>
2019-12-23 14:39:38 +0800

    aaa

--- app/Http/Controllers/Goods/GoodsController.php
+++ app/Http/Controllers/Goods/GoodsController.php
@@ -1310,10 +1310,9 @@ class GoodsController extends Controller
             $order_no1 = $xml_arr['out_trade_no'];
 //            var_dump($order_no1);die;
             $orderInfo1 = DB::table('mt_order')->where(['order_no'=>$order_no1])->first();
-//            $order_uid = DB::table('mt_user')->where(['uid'=>$orderInfo1->uid])->first();
             $order_add = DB::table('mt_order_detail')->where(['uid'=>$orderInfo1->uid,'order_no'=>$order_no1])->first();
 
-            if($order_add->order_no == NULL){
+            if($order_add->order_no){
                 $data_lists1=DB::table('mt_order_detail')->where(['uid'=>$orderInfo1->uid,'id'=>$order_add->id])->update(['order_status'=>1,'pay_price'=>$order_add->price]);
                 $data_addd=DB::table('mt_order')->where(['order_no'=>$order_no1,'uid'=>$orderInfo1->uid])->update(['order_status'=>1,'pay_price'=>$orderInfo1->price]);
                 if($data_lists1){
@@ -1335,31 +1334,33 @@ class GoodsController extends Controller
                     ];
                     return json_encode($response,JSON_UNESCAPED_UNICODE);
                 }
-            }else{
-                $data_lists=DB::table('mt_order_detail')->where(['uid'=>$orderInfo1->uid,'id'=>$order_add->id])->update(['order_status'=>1,'pay_price'=>$order_add->price]);
-//                if($order_add->order_status ==1){
-//                    $data_addd=DB::table('mt_order')->where(['order_no'=>$order_no1,'uid'=>$orderInfo1->uid])->update(['order_status'=>1,'pay_price'=>$orderInfo1->price]);
-//                }
-                if($data_addd && $data_lists){
-                    $data=[
-                        'code'=>0,
-                        'msg'=>'支付成功'
-                    ];
-                    $response = [
-                        'data'=>$data
-                    ];
-                    return json_encode($response,JSON_UNESCAPED_UNICODE);
-                }else{
-                    $data=[
-                        'code'=>1,
-                        'msg'=>'支付失败'
-                    ];
-                    $response = [
-                        'data'=>$data
-                    ];
-                    return json_encode($response,JSON_UNESCAPED_UNICODE);
-                }
             }
+//            else if($order_add->order_no){
+//                $data_lists=DB::table('mt_order_detail')->where(['uid'=>$orderInfo1->uid,'id'=>$order_add->id])->update(['order_status'=>1,'pay_price'=>$order_add->price]);
+//                $data_addd=DB::table('mt_order')->where(['order_no'=>$order_no1,'uid'=>$orderInfo1->uid])->update(['order_status'=>1,'pay_price'=>$orderInfo1->price]);
+////                if($order_add->order_status ==1){
+////                    $data_addd=DB::table('mt_order')->where(['order_no'=>$order_no1,'uid'=>$orderInfo1->uid])->update(['order_status'=>1,'pay_price'=>$orderInfo1->price]);
+////                }
+//                if($data_addd && $data_lists){
+//                    $data=[
+//                        'code'=>0,
+//                        'msg'=>'支付成功'
+//                    ];
+//                    $response = [
+//                        'data'=>$data
+//                    ];
+//                    return json_encode($response,JSON_UNESCAPED_UNICODE);
+//                }else{
+//                    $data=[
+//                        'code'=>1,
+//                        'msg'=>'支付失败'
+//                    ];
+//                    $response = [
+//                        'data'=>$data
+//                    ];
+//                    return json_encode($response,JSON_UNESCAPED_UNICODE);
+//                }
+//            }
 
 
 
