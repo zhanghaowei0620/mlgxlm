06966058db286d7fcc4ce583f610be83ee45174a
root <root@localhost.localdomain>
2019-12-23 16:55:33 +0800

    aaa

--- app/Http/Controllers/Goods/GoodsController.php
+++ app/Http/Controllers/Goods/GoodsController.php
@@ -1166,6 +1166,7 @@ class GoodsController extends Controller
                 $data['package'] = 'prepay_id=' . $array['prepay_id'];//统一下单接口返回的 prepay_id 参数值，提交格式如：prepay_id=*
                 $data['paySign'] = $this->sign($tmp);//签名,具体签名方案参见微信公众号支付帮助文档;
                 $data['out_trade_no'] = $order_id;
+                $data['is_big'] = $big;
 
             } else {
                 $data['state'] = 0;
@@ -1308,7 +1309,6 @@ class GoodsController extends Controller
             $order_no1 = $xml_arr['out_trade_no'];
             $orderInfo1 = DB::table('mt_order')->where(['order_no'=>$order_no1])->first();
             $order_add = DB::table('mt_order_detail')->where(['uid'=>$orderInfo1->uid,'order_no'=>$order_no1])->first();
-            echo 111111;die;
             if($is_big == 1){
                 $datainfo=DB::table('mt_order')->where(['order_no'=>$order_no1,'uid'=>$orderInfo1->uid])->update(['order_status'=>1,'pay_price'=>$orderInfo1->price]);
                 $data_teatil=DB::table('mt_order_detail')->where(['order_no'=>$order_no1,'uid'=>$orderInfo1->uid])->update(['order_status'=>1,'pay_price'=>$order_add->price]);
