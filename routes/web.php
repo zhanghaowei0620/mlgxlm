99e998c222dc5685b97d01bd47d5375ecaddaacb
zhangsan <884707993@qq.com>
2019-12-23 01:42:39 +0000

    h

--- routes/web.php
+++ routes/web.php
@@ -426,11 +426,6 @@ Route::post('admin_reseller_order_information','Amindbackstage\Headquarters@admi
 
 Route::post('admin_reseller_Apply_refundList', 'Amindbackstage\Headquarters@admin_reseller_Apply_refundList');   //分校订单-退款申请列表
 
-
-Route::get('admin_reseller_refund_examine', 'Amindbackstage\Headquarters@admin_reseller_refund_examine');   //分销订单-退款审核
-
-Route::post('wx_refund_Notify', 'Amindbackstage\Headquarters@wx_refund_Notify');   //微信退款-异步回调
-
 Route::get('admin_reseller_refund_examine', 'Amindbackstage\Headquarters@admin_reseller_refund_examine');   //分销订单-退款审核
 
 Route::post('wx_refund_Notify', 'Amindbackstage\Headquarters@wx_refund_Notify');   //微信退款-异步回调
